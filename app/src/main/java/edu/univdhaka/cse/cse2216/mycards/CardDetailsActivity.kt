c5aad827047f676d4ac068730a63c63403082802
Sharafat Ibn Mollah Mosharraf <sharafat_8271@yahoo.co.uk>
2018-10-27 01:20:00 +0600

    Complete Card Details screen

--- app/src/main/java/edu/univdhaka/cse/cse2216/mycards/CardDetailsActivity.kt
+++ app/src/main/java/edu/univdhaka/cse/cse2216/mycards/CardDetailsActivity.kt
@@ -6,18 +6,44 @@ import android.widget.TextView
 
 class CardDetailsActivity: Activity() {
 
-    lateinit var card: Card
+    private lateinit var card: Card
+
+    private lateinit var cardTypeLabel: TextView
+    private lateinit var bankNameLabel: TextView
+    private lateinit var cardNumberLabel: TextView
+    private lateinit var cardholderNameLabel: TextView
+    private lateinit var expiryDateLabel: TextView
+    private lateinit var cvvLabel: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setTitle(R.string.card_details)
+
         setContentView(R.layout.activity_card_details)
 
-        val cardNumberLabel = findViewById<TextView>(R.id.card_number)
-        val cardTypeLabel = findViewById<TextView>(R.id.card_type)
+        bindWidgets()
 
         card = intent.extras?.get("card") as Card
 
-        cardNumberLabel.text = card.number
+        bindValuesToLabels()
+    }
+
+    private fun bindWidgets() {
+        cardTypeLabel = findViewById(R.id.card_type_label)
+        bankNameLabel = findViewById(R.id.bank_name_label)
+        cardNumberLabel = findViewById(R.id.card_number_label)
+        cardholderNameLabel = findViewById(R.id.cardholder_name_label)
+        expiryDateLabel = findViewById(R.id.expiry_date_label)
+        cvvLabel = findViewById(R.id.cvv_label)
+    }
+
+    private fun bindValuesToLabels() {
         cardTypeLabel.text = card.type
+        bankNameLabel.text = card.bankName
+        cardNumberLabel.text = card.number
+        cardholderNameLabel.text = card.cardholderName
+        expiryDateLabel.text = card.expiryDate
+        cvvLabel.text = card.cvv.toString()
     }
 }
