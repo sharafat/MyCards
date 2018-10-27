a496fe8188c1c0a416ad7654ae314791a38fd079
Sharafat Ibn Mollah Mosharraf <sharafat_8271@yahoo.co.uk>
2018-10-27 10:02:23 +0600

    Show card type logos on card list screen

--- app/src/main/java/edu/univdhaka/cse/cse2216/mycards/activities/CardListActivity.kt
+++ app/src/main/java/edu/univdhaka/cse/cse2216/mycards/activities/CardListActivity.kt
@@ -6,10 +6,11 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.*
+import android.widget.ImageView
 import android.widget.TextView
 import edu.univdhaka.cse.cse2216.mycards.R
 import edu.univdhaka.cse.cse2216.mycards.domains.Card
-import kotlinx.android.synthetic.main.row_card_details.view.*
+import kotlinx.android.synthetic.main.row_card_list.view.*
 
 class CardListActivity : Activity() {
 
@@ -28,9 +29,9 @@ class CardListActivity : Activity() {
 
     private fun retrieveCards() {
         // TODO: fetch card list from database/server
-        cards.add(Card("VISA", "Standard Chartered Bank Ltd.", "1234 5678 9101 1123", "SHARAFAT MOSHARRAF", "12/21", 123))
+        cards.add(Card("Visa", "Standard Chartered Bank Ltd.", "1234 5678 9101 1123", "SHARAFAT MOSHARRAF", "12/21", 123))
         cards.add(Card("Master", "Eastern Bank Ltd.", "1092 1234 4485 2342", "SHARAFAT MOSHARRAF", "01/20", 456))
-        cards.add(Card("Diners Club", "Eastern Bank Ltd.", "4608 123456 7890", "SHARAFAT MOSHARRAF", "08/19", 789))
+        cards.add(Card("Discover", "Eastern Bank Ltd.", "4608 123456 7890", "SHARAFAT MOSHARRAF", "08/19", 789))
         cards.add(Card("American Express", "City Bank Ltd.", "1234 567890 3942", "SHARAFAT MOSHARRAF", "12/18", 101))
     }
 
@@ -59,7 +60,7 @@ class CardListActivity : Activity() {
     inner class CardListAdapter(private val cards : ArrayList<Card>) : RecyclerView.Adapter<CardListItemViewHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardListItemViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.row_card_details, parent, false)
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.row_card_list, parent, false)
 
             return CardListItemViewHolder(view)
         }
@@ -73,6 +74,15 @@ class CardListActivity : Activity() {
 
             holder.cardNumber.text = card.number
             holder.cardType.text = card.type
+            holder.cardTypeLogo.setImageResource(
+                    when (card.type) {
+                        "Visa" -> R.mipmap.ic_visa
+                        "Master" -> R.mipmap.ic_master
+                        "Discover" -> R.mipmap.ic_discover
+                        "American Express" -> R.mipmap.ic_amex
+                        else -> R.mipmap.ic_launcher
+                    }
+            )
 
             holder.itemView.setOnClickListener {
                 startActivity(Intent(applicationContext, CardDetailsActivity::class.java).putExtra("card", card))
@@ -85,5 +95,6 @@ class CardListActivity : Activity() {
 
         val cardNumber: TextView = view.card_number
         val cardType: TextView = view.card_type
+        val cardTypeLogo: ImageView = view.card_type_logo
     }
 }
